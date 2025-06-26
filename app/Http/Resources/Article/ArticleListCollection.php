5941bd0d85dd4b4b80283a746da298874c03f9a6
Frank.Tsai <frank.tsai@FrankTsaideMacBook-Pro.local>
2025-06-27 00:22:58 +0800

    fix: modify timezone setting to Asia/Taipei

--- app/Http/Resources/Article/ArticleListCollection.php
+++ app/Http/Resources/Article/ArticleListCollection.php
@@ -15,7 +15,7 @@ class ArticleListCollection extends ResourceCollection
                 'content'    => $article->content,
                 'image'      => $article->image ?? null,
                 'created_at' => $article->created_at->format('Y-m-d H:i:s'),
-                'updated_at' => $article->updated_at->format('Y-m-d H:i:s')
+                'updated_at' => $article->updated_at->format('Y-m-d H:i:s'),
             ];
         })->all();
     }
--- app/Http/Resources/Article/ArticleResource.php
+++ app/Http/Resources/Article/ArticleResource.php
@@ -8,13 +8,14 @@ class ArticleResource extends JsonResource
 {
     public function toArray($request): array
     {
+        
         return [
             'article_id' => $this->article_id,
             'title'      => $this->title,
             'content'    => $this->content,
-            'image'      => $this->image ?? null
-            'created_at' => $article->created_at->format('Y-m-d H:i:s'),
-            'updated_at' => $article->updated_at->format('Y-m-d H:i:s')
+            'image'      => $this->image ?? null,
+            'created_at' => $this->created_at->format('Y-m-d H:i:s'),
+            'updated_at' => $this->updated_at->format('Y-m-d H:i:s'),
         ];
     }
 }
